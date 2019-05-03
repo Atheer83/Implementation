ccc359f2dbf14b8cb019ad2d092d062d1f3a8fc5
Atheer83 <atheer.for.tech@gmail.com>
2019-05-03 17:14:31 +0300

    add index page to view features

--- Feature_Request_App/features/views.py
+++ Feature_Request_App/features/views.py
@@ -1,7 +1,13 @@
-from flask import jsonify, Blueprint
+from flask import render_template as render, jsonify, Blueprint
 
 
 features = Blueprint('feature_requests',__name__)
+
+
+@features.route('/')
+def home():
+     return render("index.html")
+
 
 #Get all features
 @features.route('/features')
@@ -20,4 +26,4 @@ def get():
             'client_priority': 2,
             'target_date': 'May 5, 2019',
             'product_area': 'Reports',
-            }])
+            }]), 200
