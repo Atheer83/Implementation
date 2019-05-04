11f413973e45f26283326dffc19ff7548b71c01d
Atheer83 <atheer.for.tech@gmail.com>
2019-05-04 18:45:41 +0300

    add routes for delete and update methods

--- Feature_Request_App/features/views.py
+++ Feature_Request_App/features/views.py
@@ -33,8 +33,42 @@ def add_feature():
 
      db.session.add(feature_req)
      db.session.commit()
-
      print(feature_schema.jsonify(feature_req))
      return feature_schema.jsonify(feature_req)
+
+# Delete a Feature
+@features.route('/features/<id>', methods=['DELETE'])
+def delete_feture(id):
+     feture = FeatureRequest.query.get(id)
+     db.session.delete(feture)
+     db.session.commit()
+
+     return feture_schema.jsonify(feture)
+
+# Update a Feature
+@features.route('/features/<id>', methods=['PUT'])
+def update_feture(id):
+  feture = FeatureRequest.query.get(id)
+
+  title = request.json['title']
+  description=request.json['description']
+  client=request.json['client']
+  # target_date=request.json['target_date']
+  # client_priority=request.json['client_priority']
+  product_area=request.json['product_area']
+
+  feture.title = title
+  feture.description = description
+  feture.client = client
+#   feture.target_date = target_date
+#   feture.client_priority = client_priority
+  feture.product_area = product_area
+
+
+  db.session.commit()
+
+  return feture_schema.jsonify(feture)
+
+     
     
     
