dd84d4cfe597ad34e48ae5bd42a07350faba7e96
Atheer83 <atheer.for.tech@gmail.com>
2019-05-05 12:29:02 +0300

    add all methods for products as module

---
+++ Feature_Request_App/static/JS/productService.js
@@ -0,0 +1,63 @@
+var ProductService = function () {
+
+    // Add Product
+    var addProduct = function (product, callback) {
+        var plainProduct = ko.toJS (product.data);
+        console.log("Saving Product [" + JSON.stringify(plainProduct) + "] ");
+        $.ajax({
+            url: "/products",
+            type: "POST",
+            data: JSON.stringify(plainProduct),
+            contentType: "application/json",
+            success: function(result) {
+                console.log(JSON.stringify(result))
+                callback (product);
+            }
+        });
+    };
+
+    // Update Product
+    var updateProduct = function (product, callback) {
+        var plainProduct = ko.toJS (product.data);
+        console.log("Updating Product [" + JSON.stringify(plainProduct) + "] ");
+        $.ajax({
+            url: "/products/" + product.data.id(),
+            type: "PUT",
+            data: JSON.stringify(plainProduct),
+            contentType: "application/json",
+            success: function(result) {
+                callback (product);
+            }
+        });
+    };
+
+    // Delete Product
+    var deleteProduct = function (product, callback) {
+        $.ajax({
+            url: "/products/" + product.data.id(),
+            type: "DELETE",
+            success: function(result) {
+                console.log(result)
+                callback (product);
+            }
+        });
+    };
+
+    // Get All Products
+    var getProducts = function (callback) {
+        $.ajax({
+            url: "/products",
+            type: "GET",
+            success: function (result) {
+                console.log("Products", JSON.stringify(result));
+                callback (result);
+            }
+        });
+    };
+    return {
+        addProduct: addProduct,
+        getProducts: getProducts,
+        deleteProduct: deleteProduct,
+        updateProduct: updateProduct,
+    };
+}();
