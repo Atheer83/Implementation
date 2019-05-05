8df9d95fe2c6cfe481032d8867d6b4871befe9ff
Atheer83 <atheer.for.tech@gmail.com>
2019-05-05 12:27:53 +0300

    add CRUD routes for products area

---
+++ Feature_Request_App/products/views.py
@@ -0,0 +1,50 @@
+from flask import  jsonify, Blueprint,request
+from Feature_Request_App import db
+from Feature_Request_App.models import Products, products_schema, product_schema
+import json
+
+
+products = Blueprint('products',__name__)
+
+#Get all products
+@products.route('/products',methods=['GET'])
+def get_products():
+     all_products = Products.query.all()
+     result = products_schema.dump(all_products)
+     return jsonify(result.data)
+
+@products.route('/products',methods=['POST'])
+def add_product():
+     name=request.json['name']
+
+     product = Products(name)
+
+     db.session.add(product)
+     db.session.commit()
+     return product_schema.jsonify(product)
+
+# Delete a product
+@products.route('/products/<id>', methods=['DELETE'])
+def delete_product(id):
+     product = Products.query.get(id)
+     db.session.delete(product)
+     db.session.commit()
+
+     return product_schema.jsonify(product)
+
+# Update a product
+@products.route('/products/<id>', methods=['PUT'])
+def update_product(id):
+  product = Products.query.get(id)
+
+  name = request.json['name']
+
+  product.name = name
+
+  db.session.commit()
+
+  return product_schema.jsonify(product)
+
+     
+    
+    
