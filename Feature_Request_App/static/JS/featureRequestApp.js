46e9b505d65ca1da78275dde76d9e2df2fccb549
Atheer83 <atheer.for.tech@gmail.com>
2019-05-05 05:13:47 +0300

    add CRUD methods in knockout view module

--- Feature_Request_App/static/JS/featureRequestApp.js
+++ Feature_Request_App/static/JS/featureRequestApp.js
@@ -6,9 +6,17 @@ var FeaturesGrid = function () {
         this.data.title = ko.observable(item.title);
         this.data.description = ko.observable(item.description);
         this.data.client = ko.observable(item.client);
-        this.data.client_priority = ko.observable(item.client_priority);
-        this.data.target_date = ko.observable(item.target_date);
+        // this.data.client_priority = ko.observable(item.client_priority);
+        // this.data.target_date = ko.observable(item.target_date);
         this.data.product_area = ko.observable(item.product_area);
+        this.displayMode = ko.observable(itemMode);
+    };
+
+    // model for client
+    clientModel = function (item, itemMode) {
+        this.data = {};
+        this.data.id = ko.observable(item.id);
+        this.data.name = ko.observable(item.name);
         this.displayMode = ko.observable(itemMode);
     };
 
@@ -18,7 +26,11 @@ var FeaturesGrid = function () {
         edit: "EDIT"
     }
 
+    // list of features
     var features = ko.observableArray();
+
+    // list of clients
+    var clients = ko.observableArray();
 
     // add a blank feature to the features array
     var addFeature = function (){
@@ -30,25 +42,57 @@ var FeaturesGrid = function () {
             // target_date: null,
             product_area: null
             };
-            features.unshift(new featureModel(item, displayMode.edit));
+            features.push(new featureModel(item, displayMode.edit));
     };
 
-    // add rquest to the client
+    // add a blank client to the clients array
+    var addClient = function (){
+        var item = {
+            name: null,
+            };
+            clients.push(new clientModel(item, displayMode.edit));
+    };
+
+    // add rquest to the feature service
     var saveFeature = function (feature) {
         FeaturesClient.addFeature(feature, saveFeatureCallback);
     }
 
+    // add rquest to the client service
+    var saveClient = function (client) {
+        ClientService.addClient(client, saveClientCallback);
+    }
+
+    // callback function for saving feature
     var saveFeatureCallback = function (feature) {
-        feature.displayMode ();
+        // feature.displayMode (displayMode.view);
+        fetchFeatures()
     };
 
-    // delete rquest to the client
+    // callback function for saving client
+    var saveClientCallback = function (client) {
+        // feature.displayMode (displayMode.view);
+        fetchClients()
+    };
+
+    // delete rquest to the feature service
     var deleteFeature = function (feature) {
         FeaturesClient.deleteFeature(feature, deleteFeatureCallback);
     }
 
+    // delete rquest to the client service
+    var deleteClient = function (client) {
+        ClientService.deleteClient(client, deleteClientCallback);
+    }
+
+    // callback function for deleting feature
     var deleteFeatureCallback = function (feature) {
         features.remove(feature);
+    };
+
+    // callback function for deleting client
+    var deleteClientCallback = function (client) {
+        clients.remove(client);
     };
 
     // edit a feature
@@ -56,27 +100,63 @@ var FeaturesGrid = function () {
         feature.displayMode(displayMode.edit)
     }
 
-    // update rquest to the client
+    // edit a client
+    var editClient = function (client) {
+        client.displayMode(displayMode.edit)
+    }
+
+    // update rquest to the featur service
     var updateFeature = function (feature) {
         FeaturesClient.updateFeature(feature, updateFeatureCallback);
     }
 
-    var updateFeatureCallback = function (feature) {
-        feature.displayMode(displayMode.view)
+      // update rquest to the client service
+      var updateClient = function (client) {
+        ClientService.updateClient(client, updateClientCallback);
     }
 
+
+    // callback function for updating feature
+    var updateFeatureCallback = function (feature) {
+        feature.displayMode(displayMode.view)
+
+    }
+
+    // callback function for updating client
+    var updateClientCallback = function (client) {
+        client.displayMode(displayMode.view)
+
+    }
+
+    // get all features
     var fetchFeatures = function () {
         FeaturesClient.getFeatures(fetchFeaturesCallback)
     }
 
+    // get all clients
+    var fetchClients = function () {
+        ClientService.getClients(fetchClientsCallback)
+    }
+
+    // callback function for fetching feature
     var fetchFeaturesCallback = function (data) {
+        features([]);
         data.forEach(function(item) {
             features.push(new featureModel(item, displayMode.view));
         });
     };
 
+    // callback function for fetching clients
+    var fetchClientsCallback = function (data) {
+        clients([]);
+        data.forEach(function(item) {
+            clients.push(new clientModel(item, displayMode.view));
+        });
+    };
+
     var init = function () {
         fetchFeatures();
+        fetchClients();
         ko.applyBindings(FeaturesGrid); 
     };
 
@@ -90,6 +170,12 @@ var FeaturesGrid = function () {
         deleteFeature,
         editFeature,
         updateFeature,
+        clients,
+        addClient,
+        saveClient,
+        deleteClient,
+        editClient,
+        updateClient
 
     };
 }();
