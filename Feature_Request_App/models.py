a76cdcefea307abc15dde338e108a13292ab5e2c
Atheer83 <atheer.for.tech@gmail.com>
2019-05-06 11:03:37 +0300

    addseed data for clients and products area

--- Feature_Request_App/models.py
+++ Feature_Request_App/models.py
@@ -9,9 +9,14 @@ class Clients(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
+    features = db.relationship('FeatureRequest', backref='client', lazy=True)
+
    
     def __init__(self,name):
         self.name = name
+
+    def __repr__(self):
+        return '%s' % self.name
 
 class ClientsSchema(ma.Schema):
     class Meta:
@@ -26,6 +31,8 @@ class Products(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
+    features = db.relationship('FeatureRequest', backref='product_area', lazy=True)
+
    
     def __init__(self,name):
         self.name = name
@@ -45,22 +52,22 @@ class FeatureRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64), nullable=False)
     description = db.Column(db.String(2000), default='')
-    client = db.Column(db.String(64), nullable=False)
     target_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    product_area = db.Column(db.String(64), nullable=False)
     client_priority = db.Column(db.Integer, nullable=False)
+    client_id = db.Column(db.Integer, db.ForeignKey('clients.id'), nullable=False)
+    product_area_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
 
-    def __init__(self,title,description,client,target_date,product_area,client_priority):
+    def __init__(self,title,description,target_date,client_priority,client_id,product_area_id):
         self.title = title
         self.description = description
-        self.client = client
         self.target_date =  target_date
-        self.product_area = product_area
         self.client_priority = client_priority
+        self.client_id = client_id
+        self.product_area_id = product_area_id
 
 class FeatureRequestSchema(ma.Schema):
     class Meta:
-        fields = ('id','title','description','client','target_date','product_area','client_priority')
+        fields = ('id','title','description','target_date','client_priority', 'client_id','product_area_id')
 
 feature_schema = FeatureRequestSchema(strict=True)
 features_schema = FeatureRequestSchema(many=True,strict=True)
@@ -69,4 +76,25 @@ features_schema = FeatureRequestSchema(many=True,strict=True)
 
 db.drop_all()
 db.create_all()
+
+clientA = Clients('ClientA')
+clientB = Clients('ClientB')
+clientC = Clients('ClientC')
+
+area1=Products('Policies')
+area2=Products('Billings')
+area3=Products('Claims')
+area4=Products('Reports')
+
+db.session.add(clientA)
+db.session.add(clientB)
+db.session.add(clientC)
+
+db.session.add(area1)
+db.session.add(area2)
+db.session.add(area3)
+db.session.add(area4)
+
+
+db.session.commit()
         
