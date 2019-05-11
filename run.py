e143191546b4fb8125b12c70316a185a7eb69d6c
Atheer83 <atheer.for.tech@gmail.com>
2019-05-11 17:48:19 +0300

    add create_app method to run the app

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-from Feature_Request_App import app
+from Feature_Request_App import db, create_app
+
+db.create_all(app=create_app())
+app = create_app()
+
 
 if  __name__ == "__main__":
     app.run(debug=True)
